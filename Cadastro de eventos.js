fee8dee24fb63dcc3e3952d28f31c2278726a553
Mittageisen <shadowhourx@gmail.com>
2021-10-31 22:23:28 -0300

    Cadastro de eventos

---
+++ Cadastro de eventos.js
@@ -0,0 +1,24 @@
+let data = 27/10/2021;
+let idadeMínima = 18;
+
+if (data <= 27/10/2021) {
+    console.log ("Evento cadastrado!")
+} else {
+    console.log ("Erro ao cadastrar evento, favor verificar a data!")
+}
+
+if (idadeMínima < 18) { 
+    console.log ("Acesso negado")
+} else {
+    console.log ("Acesso permitido, aproveite o evento!")
+}
+
+let numeroMaximoEventos = 5;
+let numeroEventosEscolhidos;
+let listaEventos = ("Evento1", "Evento2", "Evento3", "Evento4", "Evento5", "Evento6", "Evento7", "Evento8")
+
+if (numeroEventosEscolhidos <= numeroMaximoEventos) {
+    console.log ("Parabéns, você foi inscrito com sucesso!")
+} else {
+    console.log ("O número de eventos em que você se inscreveu excede o limite máximo!")
+}
